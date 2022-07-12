37c7455c7ff24cdccb129cd11610f92ca6c327e8
MBascoy <manuel.bascoy@softwareone.com>
2022-07-12 21:48:15 +0200

    Cache example

---
+++ CacheExample/CacheExample/Almacen.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Caching;
+
+namespace AlmacenApp
+{
+    internal class Almacen
+    {
+        private Cache cache;
+        public Almacen()
+        {
+            cache = new Cache();
+        }
+
+        public String ObtenerDato()
+        {
+            String dato;
+
+            dato = cache.Get("datoCadena") as String;
+
+            if (dato == null)
+            {
+                dato = ObtenerDesdeFuenteExterna();
+                cache.Add("datoCadena", dato, null, DateTime.Now.AddDays(1), TimeSpan.Zero, CacheItemPriority.High,
+                null);
+            }
+
+            return dato;
+        }
+
+        private String ObtenerDesdeFuenteExterna()
+        {
+            return "hola";
+        }
+
+
+    }
+}
---
+++ CacheExample/CacheExample/AlmacenApp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Caching;
+
+namespace AlmacenApp
+{
+    internal class AlmacenApp
+    {
+        static void Main(string[] args)
+        {
+            Almacen almacen = new Almacen();
+
+            Console.WriteLine(almacen.ObtenerDato());
+
+            Console.WriteLine(almacen.ObtenerDato());
+        }
+    }
+}
