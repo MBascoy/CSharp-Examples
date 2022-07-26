b2d193b4671eb4a3ecee7e34544f89f9a6d5a063
MBascoy <manuel.bascoy@softwareone.com>
2022-07-26 21:10:49 +0200

    StringBuilderExample

---
+++ StringBuilderExample/MonitoringLib/Recorder.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using static System.Console;
+using static System.Diagnostics.Process;
+
+namespace MonitoringLib
+{
+    public static class Recorder
+    {
+        private static Stopwatch timer = new();
+        private static long bytesPhisicalBefore = 0;
+        private static long bytesVirtualBefore = 0;
+
+        public static void Start()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+
+            bytesPhisicalBefore = GetCurrentProcess().WorkingSet64;
+            bytesVirtualBefore = GetCurrentProcess().VirtualMemorySize64;
+            timer.Restart();
+        }
+
+        public static void Stop()
+        {
+            timer.Stop();
+            long bytesPhisicalAfter = GetCurrentProcess().WorkingSet64;
+            long bytesVirtualAfter = GetCurrentProcess().VirtualMemorySize64;
+
+            WriteLine("{0:N0} physical bytes used.", bytesPhisicalAfter - bytesPhisicalBefore);
+            WriteLine("{0:N0} virtual bytes used.", bytesVirtualAfter - bytesVirtualBefore);
+            WriteLine("{0} time span ellapsed.", timer.Elapsed);
+            WriteLine("{0:N0} total milliseconds ellapsed.", timer.ElapsedMilliseconds);
+        }
+
+    }
+}
---
+++ StringBuilderExample/StringBuilderExample/StringBuilderApp.cs
@@ -0,0 +1,30 @@
+﻿using MonitoringLib;
+using static System.Console;
+
+int[] numbers = Enumerable.Range(0, 50_000).ToArray();
+
+WriteLine("String con +");
+Recorder.Start();
+
+string s = string.Empty;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    s+= numbers[i] + ", ";
+}
+
+Recorder.Stop();
+
+WriteLine("String con stringBuilder");
+
+Recorder.Start();
+
+System.Text.StringBuilder sb = new System.Text.StringBuilder();
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    sb.Append(numbers[i]);
+    sb.Append(", ");
+}
+
+Recorder.Stop();
