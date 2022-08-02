0bc7f3706782c5fbac44309c0433d5a7e26b7c59
MBascoy <manuel.bascoy@softwareone.com>
2022-08-02 20:09:46 +0200

    Async Await example

---
+++ AsyncAwaitExample/AsyncAwaitApp/Program.cs
@@ -0,0 +1,30 @@
+﻿
+ 
+public class Program
+{
+    public static async Task Main(String[] args)
+    {
+        Console.WriteLine("Inicio");
+        Task procesoA = LanzarProcesoA();
+        Task procesoB = LanzarProcesoB();
+
+        await procesoA;
+        await procesoB;
+        Console.WriteLine("terminado");
+    }
+
+    private static async Task LanzarProcesoA()
+    {
+        Console.WriteLine("ProcesoA iniciando");
+        await Task.Delay(5000);
+        Console.WriteLine("ProcesoA terminado");
+
+    }
+
+    private static async Task LanzarProcesoB()
+    {
+        Console.WriteLine("ProcesoB iniciando");
+        await Task.Delay(5500);
+        Console.WriteLine("ProcesoB terminado");
+    }
+}
